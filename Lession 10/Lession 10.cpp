6cb77b9e74ea34a6ab1a4152b277ccc565a88607
VIrus7-bit <italyfotura7@gmail.com>
2024-09-12 16:47:02 -0700

    Lession 10

---
+++ Lession 10/Lession 10.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+int main(){
+
+	using namespace std;
+
+	setlocale(LC_ALL, "Rus");
+	system("chcp 1251");
+	system("cls");
+
+	string Name;
+
+	cout << "Введите имя: "; getline(cin, Name);
+	cout << "Здравствуйте, " << Name << endl;
+
+	system("pause");
+
+	return EXIT_SUCCESS;
+}
